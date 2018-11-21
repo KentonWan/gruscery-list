a6e44e4bac74f9f15056281004aa78a68fd1a02d
Kenton Wan <kenton.wan@gmail.com>
2018-11-21 14:39:43 -0800

    referenced app in server from app

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const app = express();
+const app = require('./app');
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
