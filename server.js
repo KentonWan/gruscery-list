a9f157d3580ad8e6ad192c7c5dad7489d82f9c39
Kenton Wan <kenton.wan@gmail.com>
2018-11-20 17:00:03 -0800

    changed client/build to client/public

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/public')));
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
   });
 }
 app.listen(port, () => console.log(`Listening on port ${port}`));
