07672cd28bb1255b1c18bd5df1a1184a724c4d1e
Kenton Wan <kenton.wan@gmail.com>
2018-11-21 12:23:29 -0800

    changed routeConfig to server.js from app.js

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 
 const appConfig = require("./src/config/main-config.js");
-const routeConfig = require("./src/config/route-config.js");
 
 appConfig.init(app,express);
-routeConfig.init(app);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = require('./app');
 const port = process.env.PORT || 5000;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // API calls
 
+const routeConfig = require("./src/config/route-config.js");
+routeConfig.init(app);
 
 if (process.env.NODE_ENV === 'production') {
 
@@ -15,7 +18,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/public/index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 app.listen(port, () => console.log(`Listening on port ${port}`));
