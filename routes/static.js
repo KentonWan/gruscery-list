b6bc9880fba45d4f912e9c30759959d3f2a5b3ae
Kenton Wan <kenton.wan@gmail.com>
2018-11-21 14:26:09 -0800

    got rid of / route in staticController

--- routes/static.js
+++ routes/static.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const staticController = require("../controllers/staticController.js");
 
-router.get("/", staticController.index);
+// router.get("/", staticController.index);
 
 router.get("/api/hello", staticController.hello);
 
