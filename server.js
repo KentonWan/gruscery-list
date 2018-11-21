b173385482b7d5731301ceee7cfd583e07f029b8
Kenton Wan <kenton.wan@gmail.com>
2018-11-20 16:55:28 -0800

    resorted back to prior form

--- server.js
+++ server.js
@@ -8,11 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //API calls
-const appConfig = require("./src/config/main-config.js");
-const routeConfig = require("./src/config/route-config.js");
 
-appConfig.init(app,express);
-routeConfig.init(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
