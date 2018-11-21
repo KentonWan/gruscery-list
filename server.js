2dfa43f5cea3dd751bfcbc9803186f876dbf48a5
Kenton Wan <kenton.wan@gmail.com>
2018-11-20 16:33:13 -0800

    added in appConfig and routeConfig into server.js

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//API calls
+const appConfig = require("./src/config/main-config.js");
+const routeConfig = require("./src/config/route-config.js");
+
+appConfig.init(app, express);
+routeConfig.init(app)
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
